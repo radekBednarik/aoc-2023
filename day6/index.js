ea549df478080bf5147fb77b4024b6f4a44f7bf5
radek.Bednarik <radek.bednarik@tesena.com>
2023-12-06 09:17:43 +0100

    day 6: part one done!

---
+++ day6/index.js
@@ -0,0 +1,59 @@
+import { getInputText } from "../common/common.js";
+
+(() => {
+  const input = getInputText("day6/input.txt");
+  const [times, distances] = input.split("\n");
+  const timeVals = times
+    .split(":")[1]
+    .trim()
+    .split(" ")
+    .filter((time) => {
+      return time !== "";
+    })
+    .map((time) => {
+      return parseInt(time.trim());
+    });
+
+  const distanceVals = distances
+    .split(":")[1]
+    .trim()
+    .split(" ")
+    .filter((dist) => {
+      return dist !== "";
+    })
+    .map((dist) => {
+      return parseInt(dist.trim());
+    });
+
+  const computeAcceleration = (timePressed, accRatio = 1) => {
+    return timePressed * accRatio;
+  };
+
+  const computeDistance = (timePressed, maxTime) => {
+    const acc = computeAcceleration(timePressed);
+    return acc * (maxTime - timePressed);
+  };
+
+  let winCounts = [];
+
+  for (let a = 0; a < timeVals.length; a++) {
+    let raceWinCount = 0;
+
+    const time = timeVals[a];
+    for (let i = 0; i <= time; i++) {
+      const distance = computeDistance(i, time);
+
+      if (distance > distanceVals[a]) {
+        raceWinCount++;
+      }
+    }
+
+    winCounts.push(raceWinCount);
+  }
+
+  const result = winCounts.reduce((acc, val) => {
+    return acc * val;
+  });
+
+  console.log("Part I result: ", result);
+})();
