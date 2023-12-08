33b71c43a45098c94751a5eb116364be037afdc3
radekBednarik <radek.bednarik@tesena.com>
2023-12-08 19:59:45 +0100

    day8: does not work

--- day8/index.js
+++ day8/index.js
@@ -11,6 +11,41 @@ function getListLine(list, next) {
   })[0];
 }
 
+function navigate(navString, list, finalNode, next, counter, firstIndex) {
+  for (let i = 0; i < navString.length; i++) {
+    counter++;
+
+    const navigation = navString[i];
+
+    let index = 0;
+
+    if (navigation === "R") {
+      index = 1;
+    }
+
+    if (i === 0) {
+      next = list[firstIndex][1][index];
+      continue;
+    }
+
+    const listLine = getListLine(list, next);
+    next = listLine[1][index];
+
+    if (finalNode === next) {
+      return [next, counter];
+    }
+
+    if (i === navString.length - 1) {
+      const listLineIndex = list.findIndex((item) => {
+        const result = JSON.stringify(item) === JSON.stringify(listLine);
+        return result;
+      });
+
+      navigate(navString, list, finalNode, next, counter, listLineIndex);
+    }
+  }
+}
+
 (() => {
   const input = getInputText("day8/input.txt");
   const [navString, nodesString] = input.split("\n\n");
@@ -20,29 +55,13 @@ function getListLine(list, next) {
     .map((line) => {
       return [line[0], line[1].replaceAll(/[()]/gm, "").split(", ")];
     });
+  const finalNode = list[list.length - 1][0];
 
   let next = "";
   let counter = 0;
+  const firstIndex = 0;
 
-  for (let i = 0; i < navString.length; i++) {
-    counter++;
-    const navigation = navString[i];
+  const [_node, _counter] = navigate(navString, list, finalNode, next, counter, firstIndex);
 
-    console.log(navigation);
-
-    let index = 0;
-
-    if (navigation === "R") {
-      index = 1;
-    }
-
-    if (i === 0) {
-      next = list[0][1][index];
-      continue;
-    }
-
-    next = getListLine(list, next)[1][index];
-
-    console.log(next, counter);
-  }
+  console.log("Part II: ", _node, _counter);
 })();
